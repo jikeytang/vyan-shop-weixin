50820af91c5a84216d4ce465385c23c3ac49c649
yujianjun <mryujianjun.cn@gmail.com>
2019-07-07 15:13:37 +0800

    完成订单模块（仅静态展示）

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,15 +1,209 @@
 <template>
   <div class="container">
-    我的
+    <header class="header">
+      <div class="profile">
+        <img src="https://wwc.alicdn.com/avatar/getAvatar.do?userId=2405174948&width=80&height=80&type=sns" alt="" srcset="" class="avatar">
+        <span class="nickname">Yu</span>
+        <div class="settings">
+          <a class="setting-item" href="#"><i class="fa fa-cog fa-fw"></i></a>
+        </div>
+      </div>
+      <div class="my">
+        <a class="cell">
+          <span class="value">10</span>
+          <span>收藏夹</span>
+        </a>
+        <a class="cell">
+          <span class="value">10</span>
+          <span>关注店铺</span>
+        </a>
+        <a class="cell">
+          <span class="value">10</span>
+          <span>足迹</span>
+        </a>
+        <a class="cell">
+          <span class="value">10</span>
+          <span>红包卡劵</span>
+        </a>
+      </div>
+      <div class="slider-news">
+        <cube-slide
+          ref="slide"
+          :loop="true"
+          :initial-index="0"
+          :auto-play="true"
+          :show-dots="false"
+          :interval="2000"
+          direction="vertical"
+        >
+          <cube-slide-item>
+            <div class="news-item">
+              您的订单已发货，请确认签收。
+            </div>
+          </cube-slide-item>
+
+          <cube-slide-item>
+            <div class="news-item">
+              您有订单未评价
+            </div>
+          </cube-slide-item>
+        </cube-slide>
+      </div>
+    </header>
+    <main class="body">
+      <div class="orders">
+        <div class="order-header">
+          <span class="title">我的订单</span>
+          <router-link :to="{name: 'UserOrder'}">查看全部订单></router-link>
+        </div>
+        <div class="order-status">
+          <router-link :to="{name: 'UserOrder', query: {type: 1}}"><span class="icon"><i class="cubeic-mall"></i><span class="count">10</span></span>待付款</router-link>
+          <router-link :to="{name: 'UserOrder', query: {type: 2}}"><span class="icon"><i class="cubeic-mall"></i><span class="count">10</span></span>待发货</router-link>
+          <router-link :to="{name: 'UserOrder', query: {type: 3}}"><span class="icon"><i class="cubeic-mall"></i><span class="count">10</span></span>待收货</router-link>
+          <router-link :to="{name: 'UserOrder', query: {type: 4}}"><span class="icon"><i class="cubeic-mall"></i><span class="count">10</span></span>评价</router-link>
+          <router-link :to="{}"><span class="icon"><i class="cubeic-mall"></i><span class="count">10</span></span>待付款</router-link>
+          <router-link :to="{}"><span class="icon"><i class="cubeic-mall"></i><span class="count">10</span></span>退款/售后</router-link>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      a: 'A'
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.header {
+  background-color: #e8864c;
+  margin-bottom: 0.5rem;
+  padding: 0 0.2rem;
+  .profile {
+    align-items: flex-start;
+    display: flex;
+    height: 100%;
+    padding: 0.2rem 0.0rem;
+    .avatar {
+      width: 0.80rem;
+      height: 0.80rem;
+      border-radius: 50%;
+      border: 1px solid #fff;
+    }
 
+    .nickname {
+      padding: 0.30rem 0.20rem 0;
+      font-size: 0.4rem;
+      color: #fff;
+      flex: 1;
+    }
+
+    .settings {
+      display: flex;
+      align-items: flex-start;
+      height: 100%;
+      color: #fff;
+      font-size: 0.4rem;
+      .setting-item {
+        color: inherit;
+      }
+    }
+  }
+
+  .my {
+    display: flex;
+    .cell {
+      display: inline-block;
+      flex: 1;
+      color: #fff;
+      font-size: 100%;
+      span {
+        display: block;
+        text-align: center;
+        letter-spacing: 1px;
+      }
+      .value {
+        font-size: 150%;
+        padding-top: 0.2rem;
+        padding-bottom: 0.2rem;
+      }
+    }
+  }
+  .slider-news {
+    height: 1rem;
+    background-color: #fff;
+    border-radius: 0.2rem;
+    transform: translateY(50%);
+    overflow: hidden;
+    .news-item {
+      width: 100%;
+      height: 100%;
+      background-color: #fff;
+      padding: 0.1rem;
+    }
+  }
+}
+.body {
+  width: 100%;
+  padding: 0 0.2rem;
+  box-sizing: border-box;
+  .orders {
+    margin-top: 0.5rem + 0.2rem;
+    border-radius: 0.2rem;
+    background-color: #fff;
+    color: #666;
+    .order-header {
+      padding: 0.2rem;
+      border-bottom: 1px solid #f5f5f5;
+      display: flex;
+      align-items: center;
+      .title {
+        flex: 1;
+        font-size: 0.30rem;
+      }
+      a {
+        color: #999;
+      }
+    }
+    .order-status {
+      display: flex;
+      padding: 0.4rem 0 0.2rem;
+      align-items: center;
+      a {
+        text-align: center;
+        flex: 1;
+        display: inline-block;
+        color: #666;
+        .icon {
+          display: block;
+          font-size: 0.6rem;
+          padding-bottom: 0.1rem;
+          color: #e8864c;
+          position: relative;
+          width: 1rem;
+          margin: 0 auto;
+          .count {
+            background-color: #fff;
+            z-index: 1;
+            position: absolute;
+            top: -0.04rem;
+            right: 0.04rem;
+            display: block;
+            width: 0.3rem;;
+            height: 0.3rem;
+            line-height: 0.3rem;
+            border: 1px solid #e8864c;
+            border-radius: 50%;
+            font-size: 0.22rem;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
---
+++ src/views/user/pages/order/order-item.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="order-item">
+      <div class="order-item-header">
+        <span class="store-name">xxx旗舰店 ></span>
+        <span class="order-status">交易成功</span>
+      </div>
+      <div class="order-item-product">
+        <img src="//img13.360buyimg.com/mobilecms/s372x372_jfs/t1/11821/36/13554/133495/5c99cb1bE2f4607a6/645a9314c3ce023e.jpg!q70.dpg" class="product-pic j_rec_goods_pic opa1 ll_fadeIn">
+        <div class="product-info">
+          <div class="product-text">【送货上楼】中雀麻将机全自动过山车麻将桌餐桌式机麻免推牌调试棋牌家用机麻 灿鸿过山车【全国通用】44-136/144</div>
+          <div class="product-text product-property">balabalabalabablalbalblalbablalbalblalblalbalblalblalblababalabalabalabablalbalblalbablalbalblalblalbalblalblalblaba</div>
+          <div class="product-text">
+            <span class="product-feature">微燕购</span>
+            <span class="product-feature">七天退换</span>
+            <span class="product-feature">微燕购</span>
+            <span class="product-feature">七天退换</span>
+            <span class="product-feature">微燕购</span>
+            <span class="product-feature">七天退换</span>
+          </div>
+        </div>
+      </div>
+      <div class="product-count">
+        共1件商品 合计：¥19.90
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'order-item'
+}
+</script>
+
+<style lang="scss" scoped>
+  .order-item {
+    background-color: #fff;
+    padding: 0.2rem;
+    /*width: 100%;*/
+    box-sizing: border-box;
+    border-radius: 0.2rem;
+    overflow: hidden;
+    margin-bottom: 0.2rem;
+    .order-item-header {
+      color: #666;
+      display: flex;
+      padding: 0 0rem 0.2rem;
+      .store-name {
+        flex: 1;
+      }
+      .order-status{
+        color: #e8864c;
+      }
+    }
+    .order-item-product {
+      padding: 0.2rem 0;
+      display: flex;
+      .product-pic {
+        width: 2rem;
+        border-radius: 0.1rem;
+      }
+      .product-info {
+        flex: 1;
+        padding: 0 0.2rem;
+        .product-text {
+          margin-bottom: 0.1rem;
+          width: 100%;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          white-space: normal;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 2;
+          overflow: hidden;
+          word-break: break-word;
+        }
+        .product-property {
+          color: #999;
+        }
+        .product-feature {
+          color: #e8864c;
+          background-color: rgba(232, 134, 76, 0.1);
+          font-size: 0.12rem;
+          padding: 0rem 0.1rem;
+          margin-right: 0.1rem;
+          border-radius: 0.2rem;
+          display: inline-block;
+          margin-top: 0.1rem;
+        }
+      }
+    }
+
+    .product-count {
+      font-size: 0.12rem;
+      color: #666;
+      text-align: right;
+    }
+  }
+  /*product-pic*/
+</style>
